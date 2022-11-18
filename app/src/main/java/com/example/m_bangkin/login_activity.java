848d64701047993cf48aaf0e55bc3990f11c0250
bagas-312110485 <bagasaripradana02@gmail.com>
2022-11-18 12:14:41 +0700

    First commitv

---
+++ app/src/main/java/com/example/m_bangkin/login_activity.java
@@ -0,0 +1,49 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		login
+	 *	@date 		Sunday 13th of November 2022 12:44:35 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.m_bangkin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+
+public class login_activity extends Activity {
+
+	
+	private View _bg__login_ek2;
+	private ImageView loginmenu;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+
+		
+		_bg__login_ek2 = (View) findViewById(R.id._bg__login_ek2);
+		loginmenu = (ImageView) findViewById(R.id.loginmenu);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
