53ad4a8bfa0d128a6c63bb25bd8fdd4d116eebfe
bagas-312110485 <bagasaripradana02@gmail.com>
2022-11-18 12:14:41 +0700

    First commitv

---
+++ app/src/main/java/com/example/m_bangkin/home_activity.java
@@ -0,0 +1,49 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		home
+	 *	@date 		Sunday 13th of November 2022 12:39:48 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.m_bangkin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+
+public class home_activity extends Activity {
+
+	
+	private View _bg__home_ek2;
+	private ImageView home_ek3;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home);
+
+		
+		_bg__home_ek2 = (View) findViewById(R.id._bg__home_ek2);
+		home_ek3 = (ImageView) findViewById(R.id.home_ek3);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
---
+++ app/src/main/java/com/example/m_bangkin/iphone_13_pro_max___1_activity.java
@@ -0,0 +1,49 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		iphone_13_pro_max___1
+	 *	@date 		Sunday 13th of November 2022 01:10:42 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.m_bangkin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+
+public class iphone_13_pro_max___1_activity extends Activity {
+
+	
+	private View _bg__iphone_13_pro_max___1_ek2;
+	private ImageView tampilanmenu;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.iphone_13_pro_max___1);
+
+		
+		_bg__iphone_13_pro_max___1_ek2 = (View) findViewById(R.id._bg__iphone_13_pro_max___1_ek2);
+		tampilanmenu = (ImageView) findViewById(R.id.tampilanmenu);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
